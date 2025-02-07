8f44ac4279c25a8940480f155a330a6222394647
root <root@mehmanpashaev2006.fvds.ru>
2025-02-08 01:36:22 +0300

    Автосохранение мира 2025-02-08 01:36:22

--- backup.sh
+++ backup.sh
@@ -1,14 +1,7 @@
 #!/bin/bash
 cd /root/MinecraftServer/world
-# Остановка автосохранения на сервере
-screen -S minecraft -X stuff "save-off\n"
-screen -S minecraft -X stuff "save-all\n"
-sleep 5  # Подождем, чтобы мир сохранился
 
 # Добавление файлов в Git
 git add .
 git commit -m "Автосохранение мира $(date +'%Y-%m-%d %H:%M:%S')"
-git push origin main
-
-# Включаем автосохранение обратно
-screen -S minecraft -X stuff "save-on\n"
+git push
